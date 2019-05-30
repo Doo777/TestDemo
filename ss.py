b079d2e45f84f17506306eb50c68855a9ce837a4
Doo777 <51192803+Doo777@users.noreply.github.com>
2019-05-30 16:56:01 +0800

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,9 @@
+#config:utf-8
+import os
+path = "E:/Doo/files"
+files = os.listdir(path)
+#print(files)
+
+for f in files:
+    if f.endswith('.png') and 'fish' in f:
+        print('Look! I found this' + f)
